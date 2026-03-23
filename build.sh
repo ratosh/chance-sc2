f4f67ec3480f7f535f5d1dd0d77e05880ca10ea1
Raoni Campos <ratosh@gmail.com>
2026-03-23 17:10:23 +0100

    Update build.sh to use python3 and improve mkdir

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-python ladder_zip.py
-mkdir ./build
-unzip ./publish/Chance.zip -d ./build
+#!/bin/bash
+
+PYTHON_CMD=$(command -v python3 || command -v python)
+
+$PYTHON_CMD ladder_zip.py
+mkdir -p ./build
+unzip -o ./publish/Chance.zip -d ./build
